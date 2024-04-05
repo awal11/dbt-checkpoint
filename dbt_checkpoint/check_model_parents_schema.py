fffdafe9c6606f9034a5cc5902f8225359d0012a
awal11 <awal11@users.noreply.github.com>
2024-04-05 17:13:26 +0200

    Update check_model_parents_schema.py

--- dbt_checkpoint/check_model_parents_schema.py
+++ dbt_checkpoint/check_model_parents_schema.py
@@ -29,7 +29,8 @@ def check_parents_schema(
 
     # get manifest nodes that pre-commit found as changed
     models = get_models(manifest, filenames, include_disabled=include_disabled)
-
+    print(f"debug check_model_parents: whitelist {whitelist}")
+    
     for model in models:
         parents = list(
             get_parent_childs(
